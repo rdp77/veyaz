44d9fc1c4f6e9eedfd2eff0ad5f9e10fa172e687
rdp77 <ravidwiputra@gmail.com>
2021-06-24 16:31:08 +0700

    Update Seeder

---
+++ database/seeders/User.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
+class User extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'id' => '1',
+                'name' => 'Wreative',
+                'username' => 'admin',
+                'password' => Hash::make('admin'),
+
+            ],
+        ]);
+    }
+}
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,78 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('layouts.default')
+@section('title', __('pages.title').__(' | Dashboard'))
+@section('titleContent', __('Dashboard'))
+@section('breadcrumb', __('Tanggal ').date('d-M-Y'))
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+@section('content')
+@if(Session::has('status'))
+<div class="alert alert-danger alert-has-icon">
+    <div class="alert-icon"><i class="far fa-lightbulb"></i></div>
+    <div class="alert-body">
+        <div class="alert-title">{{ __('Informasi') }}</div>
+        {{ Session::get('status') }}
+    </div>
+</div>
+@endif
+<div class="row">
+    <div class="col">
+        <div class="card card-statistic-1">
+            <div class="card-icon bg-primary">
+                <i class="fas fa-exchange-alt"></i>
+            </div>
+            <div class="card-wrap">
+                <div class="card-header">
+                    <h4>{{ __('Total Alat Produksi') }}</h4>
+                </div>
+                <div class="card-body">
+                    {{-- {{ $production }} --}}
                 </div>
             </div>
         </div>
     </div>
-</x-app-layout>
+    <div class="col">
+        <div class="card card-statistic-1">
+            <div class="card-icon bg-danger">
+                <i class="fas fa-boxes"></i>
+            </div>
+            <div class="card-wrap">
+                <div class="card-header">
+                    <h4>{{ __('Total Perlengkapan') }}</h4>
+                </div>
+                <div class="card-body">
+                    {{-- {{ $equipment }} --}}
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card card-statistic-1">
+            <div class="card-icon bg-info">
+                <i class="fas fa-chalkboard-teacher"></i>
+            </div>
+            <div class="card-wrap">
+                <div class="card-header">
+                    <h4>{{ __('Total Persewaan Gedung') }}</h4>
+                </div>
+                <div class="card-body">
+                    {{-- {{ $rental }} --}}
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card card-statistic-1">
+            <div class="card-icon bg-success">
+                <i class="fas fa-users"></i>
+            </div>
+            <div class="card-wrap">
+                <div class="card-header">
+                    <h4>{{ __('Total Kendaraan') }}</h4>
+                </div>
+                <div class="card-body">
+                    {{-- {{ $vehicle }} --}}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
