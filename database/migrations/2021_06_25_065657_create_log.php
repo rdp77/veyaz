0a2e86f209f7084ff23781bf442ff6b4c59710a1
rdp77 <ravidwiputra@gmail.com>
2021-06-25 13:59:48 +0700

    Add Migrations

---
+++ database/migrations/2021_06_25_065657_create_log.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLog extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('log', function (Blueprint $table) {
+            $table->id();
+            $table->string('info');
+            $table->foreignId('u_id');
+            $table->string('url');
+            $table->string('user_agent');
+            $table->string('ip');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('log');
+    }
+}
