d11d49335a5b12c9cf29b305398a812778a4710d
rdp77 <ravidwiputra@gmail.com>
2021-07-13 09:01:37 +0700

    Update Credit

--- resources/views/layouts/backend/components/credit.blade.php
+++
@@ -1,6 +0,0 @@
-{{ __('Build With ') }}
-<span style="color: Tomato;">
-    <i class="fas fa-heart"></i>
-</span>
-{{ __(' | Powered By ') }}
-<a href="https://wreative.com" title="Wreative" target="_blank">{{ __('Wreative') }}</a>
