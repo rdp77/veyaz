c6491a15907e6723d643d8abc73cd9ca1098ec99
RDP77 <ravidwiputra@gmail.com>
2021-09-28 23:28:23 +0700

    :heavy_plus_sign: Add Dependency

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,5 +28,9 @@ class AppServiceProvider extends ServiceProvider
     {
         config(['app.locale' => 'id']);
         Carbon::setLocale('id');
+        // HTTPS Force
+        if (App::environment() !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
